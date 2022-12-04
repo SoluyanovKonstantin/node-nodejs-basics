00e7b886d3aef16ce384ce10d147116847d59998
Konstantin Soluianov <sol.kot15@gmail.com>
2022-12-04 21:22:41 +0400

    workers with promises

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,26 +1,31 @@
 import {Worker} from 'worker_threads'
 import os from 'os'
 
+function runService(workerData) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker('./worker.js', { workerData });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    })
+  })
+}
+
 const performCalculations = async () => {
-    let result = [];
+    let promises = [];
     for (let i = 0; i < os.cpus().length; i++) {
-        const worker = new Worker('./worker.js', { workerData: 10 + i });
-        worker.on('message', (value) => {
-            result[i] = { status: 'resolved', data: value }
-
-            if(i === os.cpus().length - 1) {
-                console.log(result);
-            }
-        })
-        worker.on('error', (value) => {
-            result[i] = { status: 'error', data: null }
-
-            if(i === os.cpus().length - 1) {
-                console.log(result);
-            }
-        })
+        promises.push(runService(i + 10).then(value => {
+            return {status: 'resolved', data: value}
+        }).catch(err => {
+            return {status: 'error', data: null}
+        }))
     }
 
+    Promise.all(promises).then(value => {
+        console.log(value)
+    })
 };
 
 await performCalculations();
